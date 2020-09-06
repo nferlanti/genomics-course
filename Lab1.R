cffdda24e783128667d142b8b83e13e2150ca4b7
Nicolas Ferlanti <nferlanti@umass.edu>
2020-09-06 22:37:04 +0000

    committing files

---
+++ Lab1.R
@@ -0,0 +1,26 @@
+##Exercise 1
+x<-3
+y<-15
+x+y
+x-y
+x*y
+x/y
+
+##Exercise 2
+x<-2
+y<-3
+x^5 > y^4
+
+##Exercise 3
+vec<-c(22,62,148,43,129)
+a<-sum(vec)
+vec/a
+
+#Exercise 4
+nucleotides<- "A,T,C,G"
+sort(nucleotides)
+
+
+
+
+
